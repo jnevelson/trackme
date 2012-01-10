821333cdaf8a91d4b54ce90de7024030ce7c3061
Jonathan Nevelson <jnevelson@gmail.com>
2012-01-09 19:40:24 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackme::Application.config.secret_token = '1398e536fbbf122a7dd01906e81e62742384b3b25f1bceefd3430f7e386bdb924079d5329f29a77a9b8b4c8d6e28494ebb3c3f29c8cc6cb338ed82c697778d5b'
